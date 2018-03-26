da3c595499de274a59f9985a974f7db94c01bfcb
MarcSherman <MarcSherman@192.168.0.22>
2018-03-26 03:02:22 -0700

    Initial Git UpLoad of Uno CardGame

---
+++ Uno2/sources/Uno2/UnoCard.java
@@ -0,0 +1,144 @@
+package Uno2;
+
+
+
+public class UnoCard {
+    
+        // Class Variables that are a) shared, and b) immutable (constants) 
+        public static final String[] RANKS = {"0", "1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6",
+                "7", "7", "8", "8", "9", "9", "Skip", "Skip", "Reverse", "Reverse",
+                "DrawTwo", "DrawTwo", "Wild", "Wild", "Wild", "Wild", "WildDraw4", "WildDraw4","WildDraw4","WildDraw4"};
+        public static final String[] COLORS = {"Yellow", "Blue", "Green", "Red", "Black"};
+
+        
+        private final int color; // 'final' keeps guys from messing with these and
+        private final int rank;        // can only assign values within a constructor
+
+    
+
+    public UnoCard() {
+        this.color=0;
+        this.rank=0;
+    }
+    
+    public UnoCard(int color, int rank) {
+        this.color= color; //5 colors: y,blue,g,r, black
+        this.rank = rank; // 27 ranks (0 plus 2 of each other card including skip, reverse, drawTwo, wild, wildDraw4)
+    }
+    
+    public int getColor() {
+        return this.color;
+    }
+
+    public int getRank() {
+        return this.rank;
+    }
+    
+ /** With a toString() method present, System.out.println will auto use toString(
+* (becuz it overrides built-in toString)   
+*/    
+    public static void printCard(UnoCard unocard) {
+        System.out.println(unocard);
+    }
+    
+    @Override
+    public String toString() {
+        String s = COLORS[this.color] + " " + RANKS[this.rank];
+        return s;
+    }
+    
+    public static boolean unoCardsMatch(UnoCard unocard1, UnoCard unocard2) { //unocard2 = PREV
+        int unoCardTgtColor = 0;
+        if (unocard2.getRank() > 24) { //Prev was wild and declared tgtColor
+            unoCardTgtColor = UnoV2.getWildColor();
+            if (unoCardTgtColor == unocard1.color) {
+                return true; // > 24 => wild or wd4
+                 }
+        }
+        if (unocard1.color > 3) {
+            return true; 
+        } else if (unocard2.color == unocard1.color || 
+                unoCardRankConversion(unocard2) == unoCardRankConversion(unocard1)) {
+            return true;
+            }
+        return false;
+    }
+    
+    /**
+     * Positive number means c1 > c2
+     * @param unocard1
+     * @param unocard2
+     * @return
+     */
+    public static int compareUnoCards(UnoCard unocard1, UnoCard unocard2) {
+        if (unoCardRankConversion(unocard1) > unoCardRankConversion(unocard2)) {
+            return 1;
+        } else if (unoCardRankConversion(unocard1) == unoCardRankConversion(unocard2)) {
+            return 0;
+        }
+        return -1;
+    }
+    
+/**
+ * Problem is there are 2 of each card, so need to convert array index number to comparable value
+ * so that, for example a yellow 5 where 5 is 1st 5 in Array index, matches second 5 that will have 
+ * higher Array index number.    
+ * @param unocard
+ * @return
+ */
+    public static int unoCardRankConversion(UnoCard unocard) {
+        int value = 0;
+        if(unocard.rank % 2 == 0) {
+            value = unocard.rank / 2;
+        } else {
+            value = unocard.rank / 2 +1;
+        }
+        return value;
+    }
+    
+    /**
+     * Card level scoring by Uno rules
+     * Called from UnoHand
+     * @param card
+     * @return
+     */
+    public static int scoreCardUno(UnoCard unocard) {
+        int cardScore = 0;
+        int color = unocard.getColor();
+        if (color > 3) {
+            cardScore = -50;
+        }
+        int rank = unocard.getRank();
+        if (rank > 18 && rank < 26) {
+            cardScore = -20;
+        } else if (rank < 20) {
+            if (rank % 2 == 0) {
+                cardScore = -rank / 2;
+            } else {
+                cardScore = -rank / 2 - 1;
+            }
+
+        }
+        return cardScore;
+    }
+    
+    public static void main(String[] args) {
+//        for(int i=0; i<4; i++) {
+//            for(int j= 19; j<= 32; j++) {
+//                System.out.println(new UnoCard(i,j));
+//            }
+//        }
+        
+        UnoCard card0000 = new UnoCard();
+        System.out.println(card0000);
+        UnoCard card0110 = new UnoCard(1,10);
+        System.out.println(card0110.getColor() + " " + card0110.getRank());
+        UnoCard card0220 = new UnoCard(2,20);
+        System.out.println("Uno card equals card0110"  +" "+ card0110);
+        System.out.println("Uno card equals card0220"  +" "+ card0220);
+        System.out.println(compareUnoCards(card0110,card0220));
+
+    }
+
+}// End class UnoCard
+
---
+++ Uno2/sources/Uno2/UnoHand.java
@@ -0,0 +1,215 @@
+package Uno2;
+
+/**
+ * 
+ */
+
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author MarcSherman
+ *
+ */
+public class UnoHand{
+    public String label;
+    public ArrayList<UnoCard> unohand;
+
+    public UnoHand(String label) {
+        this.label = label;
+        this.unohand = new ArrayList<UnoCard>();
+    }
+    
+    public UnoCard getCard(int i) {
+        return unohand.get(i);
+    }
+       
+    public String getLabel() {
+        return label;
+    }
+    
+    public int handSize() {
+        return unohand.size();
+    }
+    
+    public void swapCards(int i, int j) {
+        UnoCard temp = getCard(i);
+        unohand.set(i, getCard(j));
+        unohand.set(j, temp);
+    }
+    
+    public static UnoHand insertionSortUnoHand(UnoHand unohand) {
+        for (int i = 1; i < unohand.handSize(); i++) {
+            for(int k= i; (k>0 && (UnoCard.compareUnoCards
+               (unohand.getCard(k), unohand.getCard(k - 1)) < 1)); k--) {
+                unohand.swapCards(k,k-1);
+            }
+        }
+        return unohand;
+    }
+    
+    /**
+     * Scoring system unique to Uno is in class UnoCard
+     * */
+   public static int scoreHandUno(UnoHand unohand) {
+           int sum = 0;
+           for(int i = 0; i < unohand.handSize(); i++) {
+               UnoCard unocard = unohand.getCard(i);
+               sum += UnoCard.scoreCardUno(unocard);
+           }
+           return sum;     
+       }
+       
+   
+   public void display() {
+       System.out.println(getLabel() + ": ");
+       for (int i =0; i < handSize(); i++) {
+           System.out.println(getCard(i));
+//           System.out.println(" " + UnoCard.scoreCardUno(getCard(i)));
+       }
+       System.out.println();
+   }
+   
+   public void addCard(UnoCard unocard) {
+       unohand.add(unocard);
+   } 
+ 
+   public boolean empty() {
+       return unohand.isEmpty();
+   }
+   
+   public UnoCard popCard() { // "overloaded" removes top card, no need to shift left
+       int i = unohand.size() - 1;
+       return unohand.remove(i);
+   }
+   
+   public UnoCard popCard(int i) { // removes card[i], and shifts all cards above to left
+       return unohand.remove(i);
+   }
+ 
+   /*moves all remaining cards to the given CardCollection*/
+   public void dealAll(UnoHand that) {
+       int n = handSize();
+       deal(that, n);
+   }
+   
+   /* Deal(that,n) Removes n cards from this.CardCollection, and
+    *  adds n cards to that.CardCollection*/
+   public void deal(UnoHand that, int n) {
+       for (int i=0; i < n; i++) {
+           UnoCard card = popCard();
+           that.addCard(card);
+       }
+   }
+   
+   public void shuffle() {
+       Random rand = new Random();
+       for (int i= handSize()-1; i > 0; i--) {
+           int j = rand.nextInt(i);
+           swapCards(i,j);
+       }
+   }
+   
+   /*gets last card from calling CardCollection, but doesn't remove it*/
+   public UnoCard last() {
+       int i = handSize()-1;
+       return getCard(i);
+   }
+   
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+//        UnoDeck deck = new UnoDeck("UnoHand Test");
+//
+//        for (UnoCard unocard : deck.getUnoCards()) {
+//            System.out.print(unocard + " ");
+//            System.out.println(UnoSpecialCardsV2.unoCardWildDrawFour(unocard));
+//        }
+//        System.out.println();
+//        deck.shuffle();
+//
+//        for (UnoCard unocard : deck.getUnoCards()) {
+//            System.out.print(unocard + " ");
+//            System.out.println(UnoSpecialCardsV2.unoCardWildDrawFour(unocard));
+//        }
+//        System.out.println("");
+//        
+//        UnoHand player1;
+//        player1 = new UnoHand("Testhand1");
+//        if(player1.empty())System.out.println("empty hand 1");
+//        System.out.println("");
+//        
+//        deck.deal(player1, 7);
+//        player1.display();
+//        System.out.println(scoreHandUno(player1));
+//        
+//        UnoHand player2;
+//        player2 = new UnoHand("TestHand2");
+//        if(player2.empty())System.out.println("empty hand2");
+//        System.out.println("");
+//        
+//        deck.deal(player2, 7);
+//        UnoHand.insertionSortUnoHand(player2);
+//        player2.display();
+//        System.out.println(scoreHandUno(player2));
+//        
+//        UnoHand player3;
+//        player3 = new UnoHand("Testhand3");
+//        if(player3.empty())System.out.println("empty hand 3");
+//        System.out.println("");
+//        
+//        deck.deal(player3, 7);
+//        UnoHand.insertionSortUnoHand(player3);
+//        System.out.println(scoreHandUno(player3));
+//        player3.display();
+//        
+//        
+//        UnoHand discardpile;
+//        discardpile = new UnoHand("Discard Pile");
+//        if(discardpile.empty())
+//            System.out.println("Empty discard pile");
+//        System.out.println("");
+//        
+//        deck.deal(discardpile, 1);
+//        discardpile.display();
+//        System.out.println("");
+//        
+//        UnoHand drawpile;
+//        drawpile= new UnoHand("Draw Pile");
+//        if(drawpile.empty())
+//            System.out.println("Empty draw pile");
+//        System.out.println("");
+//        
+//        deck.dealAll(drawpile);
+//        System.out.println("Draw Pile size: " + drawpile.handSize());
+//        System.out.println("");
+       
+        for(int indexP=0;indexP < 3; indexP++) {
+            int indexNextPlayer;
+            int indexIncrement=2;
+            int playersSize=3;
+//            if (skip) {
+//                indexIncrement = 2;
+//            } else {
+//                indexIncrement = 1;
+//            }
+//            if (clockwise) {
+//                indexNextPlayer = (players.indexOf(current) + indexIncrement) % players.size();
+//            } else {
+                indexNextPlayer = (indexP - indexIncrement) % playersSize;
+                if (indexNextPlayer < 0 && indexP==1) {// need to fix this when 3 players, counter clockwise, and skip on currentplayer 1
+                        indexNextPlayer = playersSize - indexIncrement +1;// 1-2=-1 => <0 =>3-2 = 1 again
+                }else if(indexNextPlayer <0) {
+                    indexNextPlayer = playersSize-indexIncrement;
+                }
+                System.out.println(indexP + " "+ indexNextPlayer);
+            }
+            
+        }
+
+
+}
+
